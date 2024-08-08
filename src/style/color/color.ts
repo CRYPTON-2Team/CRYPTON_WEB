47d4d18fe966b3bedeaeb11be9fb49f8266d8dc6
wjzlskxk <chan2bo2@dgsw.hs.kr>
2024-08-08 22:12:56 +0900

    feat :: colorPallete

---
+++ src/style/color/color.ts
@@ -0,0 +1,22 @@
+export const CryptonColor = Object.freeze({
+  white: "#fff",
+
+  gray7: "#0f0f0f",
+  gray6: "#2f3033",
+  gray5: "#5e6166",
+  gray4: "#8d9199",
+  gray3: "#c0c4cc",
+  gray2: "#def1e5",
+  gray1: "#ebeef1",
+  gray0: "#f6f7f9",
+
+  yellow5: "#ffb700",
+  yellow4: "#ffd000",
+  yellow3: "#ffdf57",
+  yellow2: "#ffe88c",
+  yellow1: "#fff4c5",
+  yellow0: "#ffffe7",
+
+  red: "#df1d1d",
+  green: "#138636",
+});
