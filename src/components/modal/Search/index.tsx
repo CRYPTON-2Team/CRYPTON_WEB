4b2c2e8d414f16594bad367993a46d33f0ea58a8
tjduddn8511 <130065523+tjduddn8511@users.noreply.github.com>
2024-08-10 04:15:34 +0900

    Search Item Components complate

--- src/components/modal/Search/index.tsx
+++ src/components/modal/Search/index.tsx
@@ -1,17 +1,11 @@
+import Item from "./item";
 import * as S from "./style";
 
 const Search = () => {
   return (
     <S.Wrapper>
-      <S.ContentItemWrapper>
-        <S.ContetnItemMainWrapper>
-            <S.SpanWrapper>
-                <S.TitleSpan>물리</S.TitleSpan>
-                <S.AuthorSpan>서영우</S.AuthorSpan>
-            </S.SpanWrapper>
-            <S.DateSpan>2024.08.09</S.DateSpan>
-        </S.ContetnItemMainWrapper>
-      </S.ContentItemWrapper>
+      <Item />
+
     </S.Wrapper>
   );
 };
---
+++ src/components/modal/Search/item.tsx
@@ -0,0 +1,17 @@
+import * as S from "./style"
+
+const Item = () => {
+    return(
+        <S.ContentItemWrapper>
+        <S.ContetnItemMainWrapper>
+            <S.SpanWrapper>
+                <S.TitleSpan>물리</S.TitleSpan>
+                <S.AuthorSpan>서영우</S.AuthorSpan>
+            </S.SpanWrapper>
+            <S.DateSpan>2024.08.09</S.DateSpan>
+        </S.ContetnItemMainWrapper>
+      </S.ContentItemWrapper>
+    )
+}
+
+export default Item
